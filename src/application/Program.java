0a1101f89b6fa6d8ab56b2f2cd993825d54eab9b
Renato Adriano <renatobryant@gmail.com>
2021-02-22 20:48:22 -0300

    Seller and Department class

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args)  {
+		
+		Department dp=new Department(01, "Books");
+		
+		Seller sl=new Seller(21, "Bob", "bob@gmail.com", new Date(), 1500.0, dp);
+		System.out.println(sl);
+		
+	}
+
+}
