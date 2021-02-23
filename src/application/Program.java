2f8c43c15d993136d0707680d2c6ffc1b3259b36
Renato Adriano <renatobryant@gmail.com>
2021-02-22 21:12:47 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		Department dp=new Department(01, "Books");
 		
 		Seller sl=new Seller(21, "Bob", "bob@gmail.com", new Date(), 1500.0, dp);
+		
+		SellerDao sellerDao= DaoFactory.createSellerDao();
+		
 		System.out.println(sl);
 		
 	}
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,8 +1,6 @@
 package model.dao;
 
 import java.util.List;
-
-import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -10,7 +8,7 @@ public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteById(Integer id);
-	Department findById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 
 }
